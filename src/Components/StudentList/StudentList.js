d963439c149094e8e5bbf33d44de6277c8c33ce6
haideralishah <headeralishah@gmail.com>
2020-01-22 18:00:42 +0500

    INITIAL COMMIT

---
+++ src/Components/StudentList/StudentList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View , Text} from 'react-native'
+
+const StudentList  = ()=>{
+    return(
+        <View>
+            <Text>StudentList</Text>
+        </View>
+    )
+}
+
+export { StudentList }
